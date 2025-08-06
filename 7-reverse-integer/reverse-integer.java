4008403b226f93c8526d66cb5c64424af20cfa3b
mdhanush96 <m.dhanush0416@gmail.com>
2025-08-06 21:07:27 +0530

    Time: 1 ms (81.57%) | Memory: 41.3 MB (23.68%) - LeetSync

--- 7-reverse-integer/reverse-integer.java
+++ 7-reverse-integer/reverse-integer.java
@@ -3,13 +3,13 @@ class Solution {
         long reminder = 0;
         int num = 0;
         while(x!=0){
-            num = x%10;
+            // num = x%10;
             // if(reminder > Integer.MAX_VALUE/10 || (reminder > Integer.MAX_VALUE && num > 7)){
             //     return 0;
             // }
             // else if (reminder < Integer.MIN_VALUE / 10 || (reminder<Integer.MIN_VALUE && num < -8)) return 0;
             
-            reminder = reminder * 10 + num;
+            reminder = reminder * 10 + x%10;
             x = x/10;
             if(reminder > Integer.MAX_VALUE || reminder < Integer.MIN_VALUE) return 0;
         }
